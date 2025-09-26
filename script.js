fdf4f801bf7fb48e0fc4cf06f8702532eb485c42
shubh <shubhamsharmajcc@gmail.com>
2025-09-27 01:56:35 +0530

    Initial commit with index.html, script.js, and styles.css

---
+++ script.js
@@ -0,0 +1,40 @@
+const increment = document.getElementById('increment');
+const decrement = document.getElementById('decrement');
+const clear = document.getElementById('clear');
+const counter = document.getElementById('counter');
+const error = document.getElementById('error')
+
+let count = 1
+counter.textContent = count;
+
+increment.addEventListener('click', () => {
+    count++;
+    counter.textContent = count;
+    error.style.display = 'none'
+    clear.style.display = 'block';
+    decrement.disabled = false;
+})
+
+function clearFn() {
+    count = 0;
+    counter.textContent = count;
+    error.style.display = 'block'
+    clear.style.display = 'none';
+    decrement.disabled = true;
+}
+
+
+decrement.addEventListener('click', () => {
+    if (count === 1 || count === 0) {
+        clearFn()
+    }
+
+    if(count > 1){
+        count--;
+        counter.textContent = count;
+    }
+})
+
+clear.addEventListener('click', () => {
+    clearFn()
+})
